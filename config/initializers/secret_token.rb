11e740ad29b9ee55d19cd8a2f890b9e492c59cb3
Rahoul Datta <rahouldatta@gmail.com>
2013-04-13 02:30:06 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentalConstructs::Application.config.secret_token = '7c1efa8662bdcc3eb0e88976193451edfae1f5a9788acc0292782fb87274071decad3c300fa33a28741aaa7863625632441f01c4fc0f09ff59fd0b3bd6bc7ae3'
