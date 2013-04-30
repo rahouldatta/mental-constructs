f12fe293268c662c4aa52e403f047e72db74c0ab
Rahoul Datta <rahouldatta@gmail.com>
2013-05-01 04:46:08 +0530

    upgrades

--- app/controllers/howl_controller.rb
+++ app/controllers/howl_controller.rb
@@ -3,16 +3,16 @@ class HowlController < ApplicationController
   layout 'howl'
 
   def index
-    @howls = Howl.order("created_at DESC").limit(50)
+    @howls = Howl.order("created_at DESC").limit(52)
   end
 
   def update_howl_display
-    @howls = Howl.order("created_at DESC").limit(50)
+    @howls = Howl.order("created_at DESC").limit(52)
   end
 
   def record_howl
     h = Howl.create(:howl => params[:howl])
-    @howls = Howl.order("created_at DESC").limit(50)
+    @howls = Howl.order("created_at DESC").limit(52)
   end
 
 end
---
+++ config/initializers/howler_types.rb
@@ -0,0 +1,3 @@
+HOWLER_TYPES = ["Tortured Soul", "Ominous Thinker",
+                "Dark Dweller", "Frustrated",
+                "Burdened", "Melancholic"]
