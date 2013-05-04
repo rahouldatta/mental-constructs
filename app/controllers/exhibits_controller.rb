7350159ac7931322286af4ffbb366831769928bd
Rahoul Datta <rahouldatta@gmail.com>
2013-05-05 02:57:22 +0530

    search and other upgrades

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -4,8 +4,29 @@ class ExhibitsController < ApplicationController
   layout 'exhibit'
 
   def index
+  #  @exhibits = Exhibit.order("created_at desc").paginate(:page=>params[:page], :per_page=>13)
+  #end
+  if session[:specific_thinker].nil? and session[:exhibit_search_results].nil?
     @exhibits = Exhibit.order("created_at desc").paginate(:page=>params[:page], :per_page=>13)
+  elsif !session[:exhibit_search_results].nil? and session[:specific_thinker].nil?
+    search_preparation
+  else
+    @exhibits = Exhibit.where(:thinker => session[:specific_thinker]).order("created_at desc").paginate(:page=>params[:page], :per_page=>13)
+    @thinker = session[:specific_thinker]
+    session[:specific_thinker] = nil
   end
+end
+
+def search_preparation
+  @exhibits = []
+  if session[:exhibit_search_results] == "Nothing Found"
+    @search_result = 0
+  else
+    session[:exhibit_search_results].each {|id| @exhibits << Exhibit.find(id)}
+    @search_result = @exhibits.count
+  end
+  session[:exhibit_search_results] = nil
+end
 
   def show
     @exhibit = Exhibit.find(params[:id])
@@ -14,8 +35,10 @@ class ExhibitsController < ApplicationController
   end
 
   def search_exhibits
-    puts params
-    puts "==============="
+    search_results = Exhibit.new.search_for_exhibits(params[:search_parameter])
+    session[:search_parameter] = params[:search_parameter]
+    search_results.blank? ? session[:exhibit_search_results] = "Nothing Found" : session[:exhibit_search_results] = search_results
+    redirect_to exhibits_path
   end
 
 end
--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -1,3 +1,6 @@
+require File.join(Rails.root, 'lib/engines/searcher.rb')
+include Searcher
+
 class Exhibit < ActiveRecord::Base
   attr_accessible :brain_storm, :construct, :foot_notes, :popularity_quotient, :thinker, :title
 
@@ -7,6 +10,14 @@ class Exhibit < ActiveRecord::Base
 
   def treatment_exhibit
     self.popularity_quotient = 0
+  end
+
+  def search_for_exhibits(search_string)
+    found = []
+    Exhibit.all.each do |exhibit|
+      found << exhibit.id if exists_somewhere?(exhibit.construct,search_string) or exists_somewhere?(exhibit.title,search_string) or exists_somewhere?(exhibit.thinker,search_string)
+    end
+    found
   end
 
 end
